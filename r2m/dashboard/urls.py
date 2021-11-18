3359e6dc7b6a908e20055892fa9ce71b994dee2d
Antoine Smaguine <antoine.smaguine@gmail.com>
2021-11-18 10:42:39 +0100

    upload

--- r2m/dashboard/urls.py
+++ r2m/dashboard/urls.py
@@ -3,18 +3,18 @@ from . import views
 
 urlpatterns = [
     #Lien vers la page de connection de l'application
-    path('login/', views.loginPage, name='login'),
+    #path('login/', views.loginPage, name='login'),
     #Lien vers la page de déconexion de l'application
-    path('logout/', views.logoutUser, name='logout'),
+    #path('logout/', views.logoutUser, name='logout'),
 
     path('', views.home, name='home'),
-    path('clientsbase/', views.clientsBasepage, name='clientsbase'),
-    path('client/', views.clientPage, name='client-page'),
-    path('videobase/', views.videoBasepage, name='videobase'),
-    path('video/', views.videoBasepage, name='video-page'),
+    #path('clientsbase/', views.clientsBasepage, name='clientsbase'),
+    #path('client/', views.clientPage, name='client-page'),
+    #path('videobase/', views.videoBasepage, name='videobase'),
+    #path('video/', views.videoBasepage, name='video-page'),
 
-    path('customer/<str:pk>/', views.customer, name='customer'),
-    path('create_order/<str:pk>/', views.createOrder, name='create_order'),
-    path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
-    path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
+    #path('customer/<str:pk>/', views.customer, name='customer'),
+    #path('create_order/<str:pk>/', views.createOrder, name='create_order'),
+    #path('update_order/<str:pk>/', views.updateOrder, name='update_order'),
+    #path('delete_order/<str:pk>/', views.deleteOrder, name='delete_order'),
 ]
--- r2m/dashboard/views.py
+++ r2m/dashboard/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request): 
+    context = {'title': 'test'}
+    return render(request, 'dashboard/main.html', context)
